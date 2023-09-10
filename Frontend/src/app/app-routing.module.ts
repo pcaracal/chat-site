6ce75f04bd0013210ea5c0b35b2b546e0eda6b12
pcaracal <peter.schreivogel@stud.kbw.ch>
2023-09-10 10:48:33 +0200

    feat: channel routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {OverviewComponent} from "./overview/overview.component";
+import {ChannelComponent} from "./channel/channel.component";
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'overview', component: OverviewComponent}
+  {path: 'overview', component: OverviewComponent},
+  {path: 'channel/:id', component: ChannelComponent}
 ];
 
 @NgModule({
--- Frontend/src/app/channel/channel.component.ts
+++ Frontend/src/app/channel/channel.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-channel',
   templateUrl: './channel.component.html',
   styleUrls: ['./channel.component.scss']
 })
-export class ChannelComponent {
+export class ChannelComponent implements OnInit {
+  channelId: number = -1;
 
+  constructor(private route: ActivatedRoute, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    const isLoggedIn = sessionStorage.getItem("Authorization") !== null;
+    if (!isLoggedIn) {
+      this.router.navigate(["/"]);
+    }
+
+    this.route.params.subscribe(params => {
+      this.channelId = params['id'];
+    });
+  }
+
+  getChannelData() {
+    // TODO: 1. See if channel exists and user has access to it
+    // TODO: 2. Get channel data if yes, else redirect to overview
+    // TODO: 3. Display channel data
+    // TODO: 4. Display channel messages
+    // TODO: 5. Display channel users
+    // TODO: 6. Display channel settings if user is admin
+  }
 }
