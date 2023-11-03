7661b18f60d6bf405a974e26494eda83374e8c9c
pcaracal <peter.schreivogel@stud.kbw.ch>
2023-11-03 13:50:08 +0100

    fix: speed up argon2id algorithm

--- Backend/BackendApp/Repositories/LoginRepository.cs
+++ Backend/BackendApp/Repositories/LoginRepository.cs
@@ -12,13 +12,15 @@ public class LoginRepository : ILoginRepository {
   private readonly IConfiguration _configuration;
   private readonly ApplicationDbContext _context;
 
-  public LoginRepository(ApplicationDbContext context, IConfiguration configuration) {
+  public LoginRepository(ApplicationDbContext context,
+                         IConfiguration configuration) {
     _context = context;
     _configuration = configuration;
   }
 
   public bool UserExists(string username) {
-    return _context.user.FirstOrDefault(_login => _login.username == username) != null;
+    return _context.user.FirstOrDefault(_login => _login.username ==
+                                                  username) != null;
   }
 
   public Login GetUser(string username) {
@@ -35,7 +37,7 @@ public class LoginRepository : ILoginRepository {
     string _out;
     using (var hasher = new Argon2id(Encoding.UTF8.GetBytes(password))) {
       hasher.DegreeOfParallelism = 1;
-      hasher.Iterations = 40;
+      hasher.Iterations = 10;
       hasher.MemorySize = 19456;
       _out = Convert.ToBase64String(hasher.GetBytes(128));
     }
@@ -45,8 +47,10 @@ public class LoginRepository : ILoginRepository {
 
   // Helper method to generate a JWT token
   public string GenerateJwtToken(string username) {
-    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
-    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+    var securityKey = new SymmetricSecurityKey(
+        Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+    var credentials =
+        new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
     var claims = new[] {
       new Claim(JwtRegisteredClaimNames.Sub, username),
@@ -54,12 +58,9 @@ public class LoginRepository : ILoginRepository {
     };
 
     var token = new JwtSecurityToken(
-      _configuration["Jwt:Issuer"],
-      _configuration["Jwt:Audience"],
-      claims,
-      expires: DateTime.UtcNow.AddHours(1), // Token expiration time
-      signingCredentials: credentials
-    );
+        _configuration["Jwt:Issuer"], _configuration["Jwt:Audience"], claims,
+        expires: DateTime.UtcNow.AddHours(1), // Token expiration time
+        signingCredentials: credentials);
 
     return new JwtSecurityTokenHandler().WriteToken(token);
   }
